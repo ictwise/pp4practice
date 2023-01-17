dac073217c2dab0c12d2c651eb8f1c06c1e89f64
ictwise <57628753+ictwise@users.noreply.github.com>
2023-01-17 17:16:19 +0000

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ecobiketech.wsgi:application
---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+os.environ["STRIPE_PUBLIC_KEY"] = "pk_test_51MMIerG8VXdhRAoCcwm3v6l94ead0XfQiABcLMsM6gT4OsLlJN4gTCd7J8eqqA65cdQ3ukvQWtoFfNM9FCxj5tLc00v9OWKf74"
+os.environ["STRIPE_SECRET_KEY"] = "sk_test_51MMIerG8VXdhRAoCoIfWaqIwUebRAqdQmjKspOAdPJdxwG1NV9f45gREJfMNqyfqYGFx0yc6Yr5edTHP1TMD8tUy00oeHzyKfa"
+os.environ["SECRET_KEY"] = "mehtni6-n1#q7^1gkf-=!e)caamrv8kyf5_f4t_9dvazixlh+*"
+os.environ["STRIPE_WH_SECRET"] = "whsec_StWyl8qbLpRZfr9vbAZa2RKSZFnpAJtX"
+os.environ.setdefault('DEVELOPMENT', '1')
